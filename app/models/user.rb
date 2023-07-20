4f4c81c4d82eaa21f3bdd11090821879c469debf
Takagi <zkazo@gmx.com>
2023-07-20 16:23:01 +0900

    誤字の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :profilea, presence: true
+  validates :profile, presence: true
   validates :occupationa, presence: true
   validates :position, presence: true
 
