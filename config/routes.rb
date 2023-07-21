4ef1ae5b41b266225371e12b75ffc3f2fb251d43
Takagi <zkazo@gmx.com>
2023-07-21 17:04:15 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'prototypes#index'
   devise_for :users
 
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: :show
 end
