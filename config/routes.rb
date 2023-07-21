ecd4fe792d4f918e69df10655fd03ed2c09d6d88
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-21 19:45:01 +0900

    ルーティングにcommentモデルのcreateアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'prototypes#index'
   devise_for :users
 
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show] do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
