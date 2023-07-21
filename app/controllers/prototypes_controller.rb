436c12de8a83ff7616e1fc781aa4ef72cbc4c83a
Takagi <zkazo@gmx.com>
2023-07-21 15:57:57 +0900

    prototype詳細機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,4 +2,8 @@ class PrototypesController < ApplicationController
     def index
         @prototypes = Prototype.all
     end
+
+    def show
+        @Prototype = Prototype.find(params[:id])
+      end
 end
