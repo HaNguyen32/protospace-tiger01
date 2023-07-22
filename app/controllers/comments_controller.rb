be519ee17e3948a92ee63383a45fcff7b55e5391
Takagi <zkazo@gmx.com>
2023-07-22 16:55:15 +0900

    エラー修正,コメント投稿できるようになった

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
 class CommentsController < ApplicationController
     def create
-        Comment.create(comment_params)
-        redirect_to "/prototypes/#{comment.prototype_id}"
+        @comment = Comment.new(comment_params)
+        #binding.pry
+        if @comment.save
+            redirect_to prototype_path(@comment.prototype_id) 
+        else
+            redirect_to prototype_path(params[:prototype_id]), alert: 'コメントの保存に失敗しました'
+        end
+        #Comment.create(comment_params)
+        #redirect_to "/prototypes/#{comment.prototype_id}"
     end
 
 
     private
     def comment_params
-        params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+        #params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+        params.require(:comment).permit(:comment).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
     end
 end
