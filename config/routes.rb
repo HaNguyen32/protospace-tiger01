79d2de6c289fe77c139f5d291b0c92ffa3079059
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-22 21:56:57 +0900

    ルーティングにedit&updateを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'prototypes#index'
   devise_for :users
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy] do
+  resources :prototypes do
     resources :comments, only: :create
   end
   resources :users, only: :show
